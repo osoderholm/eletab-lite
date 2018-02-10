585e50fd925bb5354d691afeedcc1fce1cb9af9d
Anton Lindholm <anton.c.e.lindholm@gmail.com>
2018-02-10 16:54:29 +0200

    Add flags and smart env vars

--- cmd/eletab/main.go
+++ cmd/eletab/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"path"
+
+	"github.com/jamiealquiza/envy"
 
 	"github.com/gorilla/mux"
 	"github.com/osoderholm/eletab-lite/bundles/apibundle"
@@ -13,9 +15,14 @@ import (
 )
 
 func main() {
-	port := string(os.Getenv("ELETAB_PORT"))
-	appPath := string(os.Getenv("ELETAB_PATH"))
-
+	var port = flag.Int("port", 8080, "port ")
+	var address = flag.String("address", "127.0.0.1", "address to listen on")
+	var staticfiles = flag.String("staticfiles", "../../", "path to all static files") // TODO: Include these in the binary
+	envy.Parse("ELETAB")                                                               // looks for ELETAB_PORT and ELETAB_STATICFILES
+	flag.Parse()
+	if *port <= 0 {
+		panic("port is fuked")
+	}
 	a := authbundle.Init()
 
 	r := mux.NewRouter()
@@ -31,13 +38,15 @@ func main() {
 	apiSR.HandleFunc("/client_login", apiCtrl.HandleClientLogin).Methods(http.MethodPost)
 	apiSR.HandleFunc("/account_login", apiCtrl.HandleAccountLogin).Methods(http.MethodPost)
 
-	log.Println(path.Join(appPath, "/static/"))
-	staticFileDirectory := http.Dir(path.Join(appPath, "/static/"))
+	sp := path.Join(*staticfiles, "/static/")
+	log.Printf("Static path: %s\n", sp)
+	staticFileDirectory := http.Dir(sp)
 	staticFileHandler := http.StripPrefix("/", http.FileServer(staticFileDirectory))
 	r.PathPrefix("/").Handler(staticFileHandler)
 
 	http.Handle("/", r)
-	fmt.Println("Server running on port", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	sck := fmt.Sprintf("%s:%d", *address, *port)
+	fmt.Printf("We are up and running at %s!\n", sck)
+	log.Fatal(http.ListenAndServe(sck, nil))
 
 }
